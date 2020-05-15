31eae733427e25443854ac77e8ddf3e7700a3738
Yee-Ting Li <yee379@gmail.com>
2020-05-14 20:47:59 -0700

    add end for file write

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-//const git = require('nodegit');
 const path = require('path');
 const app = express();
 const fs = require('fs');
@@ -22,10 +21,8 @@ const repoPath = "/tmp/master/";
 const dbPath = 'data/papers.json';
 
 // set the ssh key to use
-//const ssh_key=process.env.SSH_KEY;
 console.log( "Using "  + process.env.GIT_SSH_COMMAND );
 exec( "git --version" );
-//process.env.GIT_SSH_COMMAND = "ssh -i " + ssh_key
 
 
 // helper functions
@@ -36,44 +33,9 @@ var rmdir = ( dir ) => {
 	})	
 };
 
-// create new branch branchName at repo path branchPath and send a pull request
-// var submit = ( branchName, branchPath ) => {
-// 	var dir = path.resolve( branchPath );
-// 	// duplicate master
-// 	rmdir( dir );
-// 	fse.copySync( repoPath, dir );
-// 	console.log("creating new branch " + branchName + " at " + dir );
-//
-// 	return git.Repository.open( dir )
-// 	.then( (repo) => {
-// 		return repo.getHeadCommit()
-// 		.then( (commit) => {
-// 			return repo.createBranch( branchName, commit, 0 )
-// 			.then( (ref) => {
-// 				// console.log(ref);
-// 				// return repo.checkoutRef(ref);
-// 				repo.checkoutRef(ref);
-// 				return repo;
-// 			})
-// 		})
-// 	})
-// }
-
 // grab newest db from repoUrl and read into memory
 console.log("Cloning " + repoUrl + " to " + repoPath );
 rmdir( repoPath );
-//var repo = git.Clone( repoUrl, repoPath, {} )
-// .then( (repo) => {
-// 	console.log("cloned!");
-// 	const repoDbPath = repoPath + dbPath;
-// 	console.log("loading " + repoDbPath );
-// 	db = JSON.parse(fs.readFileSync( repoDbPath, 'utf8', (err) => {
-// 		if (err) {
-// 			console.err(err);
-// 		}
-// 	}));
-// 	//console.log(db);
-// })
 
 exec( "git clone " + repoUrl + " " + repoPath);
 const repoDbPath = repoPath + dbPath;
@@ -132,7 +94,8 @@ var create_pr = ( branchName, newdb ) => {
     // create PR
     // rmdir( dir );
   });
-  d.write( JSON.stringify(newdb, null, 2) )
+  d.write( JSON.stringify(newdb, null, 2) );
+  d.end();
 
 	process.chdir( repoPath );
 }
@@ -177,37 +140,4 @@ app.post( '/api/paper/:id', (req, res) => {
 	res.status(200).send(db2[id]);
 
 })
-
-// update paper
-// app.post( '/paper/:id', (req, res) => {
-//
-// 	var id = req.params.id;
-//
-// 	// modify entry
-// 	var db2 = JSON.parse( JSON.stringify(db) );
-// 	db2[id] = req.body;
-//
-// 	var branchName = 'other';
-// 	var branchPath = '/tmp/' + branchName + '/';
-// 	submit( branchName, branchPath )
-// 	.catch( (err) => {
-// 		console.error(err);
-// 		res.status(500);
-// 	})
-// 	.done( () => {
-//
-// 		// make changes to db in new checkout
-// 		const newdbPath = branchPath + '/' + dbPath;
-// 		const str = JSON.stringify(db2, null, 2)
-// 		fs.createWriteStream( newdbPath ).write( str );
-//
-// 		// TODO: submit PR to remote
-//
-// 		// rmdir( newRepo );
-// 		res.status(200).send(db2[id]);
-//
-// 	})
-//
-// })
-
 
